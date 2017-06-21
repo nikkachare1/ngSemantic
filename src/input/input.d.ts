a630db02575a8f6f4d63dd02231b58676f573b27
nikkachare1 <nikhilkachare01@gmail.com>
2017-06-21 11:45:57 +0530

    inputType and clasType made public

--- src/input/input.d.ts
+++ src/input/input.d.ts
@@ -10,7 +10,7 @@ export declare class SemanticInputComponent implements OnInit {
     model: {};
     control: FormControl;
     modelChange: EventEmitter<string | number>;
-    private isInsideForm;
+    isInsideForm;
     constructor(viewRef: ViewContainerRef);
     ngOnInit(): void;
     inForm(el: Node, classname: string): boolean;
@@ -22,8 +22,8 @@ export declare class SemanticCheckboxComponent {
     value: string | number;
     name: string;
     type: string;
-    private inputType;
-    private classType;
+    inputType;
+    classType;
 }
 export declare class SemanticTextareaComponent {
     control: FormControl;
